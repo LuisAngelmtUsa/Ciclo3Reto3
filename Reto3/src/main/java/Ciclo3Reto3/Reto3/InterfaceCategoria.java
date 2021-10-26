614b3993a0f7f605f740149172361f05e70a042c
LuisAngelVO <81832516+LuisAngelVO@users.noreply.github.com>
2021-10-25 19:09:03 -0500

    firs commit

---
+++ Reto3/src/main/java/Ciclo3Reto3/Reto3/InterfaceCategoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Ciclo3Reto3.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author LUIS ANGEL VILLARREAL ORTIZ G28
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer>{
+    
+}
